4dc0f99520ba3242b351aa34acd3a8f11872272c
Michael <mkleyster@mail.ua>
2017-02-01 16:48:14 +0200

    add image1

--- authoresetion.php
+++ authoresetion.php
@@ -1,1 +1,10 @@
 <?php
+$login= $_POST['login'];
+$pass= $_POST['pass'];
+if($login==='admin' || $pass==='admin'){
+    $message= "Всё верно ))";
+    
+}else{
+    $message= "Голяк ((";
+}
+echo $message;
